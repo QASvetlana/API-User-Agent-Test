3a23228f783f79e310f2b3362e8332228d8437a5
Sveta Andreeva <89111378147v@yandex.com>
2022-11-19 18:17:33 +0300

    create test for user agent

--- src/test/java/UserAgentTest.java
+++ src/test/java/UserAgentTest.java
@@ -1,25 +1,50 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserAgentTest {
-    @Test
-    public void testUserAgent() {
-        String name = "Username";
+    @ParameterizedTest
+    @CsvSource(value = {"'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30', Mobile, No, Android",
+            "'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1', Mobile, Chrome, iOS",
+            "'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', Googlebot, Unknown, Unknown",
+            "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0', Web, Chrome, No",
+            "'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1', Mobile, No, iPhone"
+    })
+    void ex13Test(String user_agent, String platform, String browser, String device) {
+
+        Map<String, String> query = new HashMap<>();
+        query.put("user_agent", user_agent);
+        query.put("platform", platform);
+        query.put("browser", browser);
+        query.put("device", device);
 
         JsonPath response = RestAssured
                 .given()
-                .queryParam("Mozilla\\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\\/537.36 (KHTML, like Gecko) Chrome\\/107.0.0.0 Safari\\/537.36\",\"platform\":\"Web\",\"browser\":\"Chrome\",\"device\":\"No\"", name)
-                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .header("User-Agent", user_agent)
+                .get("https://playground.learnqa.ru/api/user_agent_check")
                 .jsonPath();
+        response.prettyPrint();
 
-        String answer = response.getString("user_agent");
-        System.out.println(answer);
+        String platformRes = response.getJsonObject("platform");
+        String browserRes = response.getJsonObject("browser");
+        String deviceRes = response.getJsonObject("device");
 
-       // assertEquals("user_agent + name, answer,")
-
-
+        System.out.println(user_agent);
+        if (!platform.equals(platformRes)) {
+            System.out.println("Неверная платформа: " + platform + "; ожидаемая: " + platformRes);
+        }
+        if (!browser.equals(browserRes)) {
+            System.out.println("Неверный браузер: " + browser + "; ожидаемый: " + browserRes);
+        }
+        if (!device.equals(deviceRes)) {
+            System.out.println("Неверный девайс: " + platform + "; ожидаемый: " + deviceRes);
+        }
+        assertTrue((platform.equals(platformRes)) && (browser.equals(browserRes)) && (device.equals(deviceRes)), "Ошибка");
     }
+
 }
+
